871dd0ba0ea3bf093ca67aedf8fb147f7c50d89a
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-06 07:32:41 +0700

    Create BootReceiver.kt

---
+++ app/src/main/java/com/nslocal/games/game/BootReceiver.kt
@@ -0,0 +1,11 @@
+package com.nslocal.games
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(ctx: Context?, intent: Intent?) {
+        // Auto start on boot if enabled
+    }
+}
