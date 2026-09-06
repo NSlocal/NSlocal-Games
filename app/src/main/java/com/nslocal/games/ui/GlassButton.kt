109dc0220e06caf529ed5d213061f3a3f1213f87
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-06 10:09:13 +0700

    Update GlassButton.kt

--- app/src/main/java/com/nslocal/games/ui/GlassButton.kt
+++ app/src/main/java/com/nslocal/games/ui/GlassButton.kt
@@ -3,6 +3,7 @@ package com.nslocal.games.ui
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatButton
+import com.nslocal.games.R
 
 class GlassButton @JvmOverloads constructor(
     ctx: Context, attrs: AttributeSet? = null, defStyle: Int = 0
@@ -10,5 +11,7 @@ class GlassButton @JvmOverloads constructor(
     init {
         setBackgroundResource(R.drawable.glass_bg)
         setTextColor(android.graphics.Color.WHITE)
+        setPadding(32, 16, 32, 16)
+        textSize = 14f
     }
 }
