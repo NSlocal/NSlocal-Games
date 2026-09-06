07084b7ebdb61e2563b8a1b8ba5a5a64b7d03cfd
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-06 12:39:19 +0700

    Update BoostEngine.kt

--- app/src/main/java/com/nslocal/games/optimizer/BoostEngine.kt
+++ app/src/main/java/com/nslocal/games/optimizer/BoostEngine.kt
@@ -1,39 +1,23 @@
 package com.nslocal.games.optimizer
 
 import android.content.Context
-import android.os.PowerManager
 import com.nslocal.games.perf.PerformanceHelper
 import com.nslocal.games.perf.ThermalCoolingManager
 
 class BoostEngine(private val ctx: Context) {
     private val perfHelper = PerformanceHelper(ctx)
     val thermalManager = ThermalCoolingManager(ctx)
-    private val powerManager = ctx.getSystemService(Context.POWER_SERVICE) as PowerManager
-    private var wakeLock: PowerManager.WakeLock? = null
 
     fun applyAll() {
-        if (perfHelper.isQualcomm) applyQualcommTweaks()
-        if (perfHelper.isMediaTek) applyMediatekTweaks()
-        applyBatteryStabilizer()
+        if (perfHelper.isQualcomm) applyQualcomm()
+        if (perfHelper.isMediaTek) applyMediatek()
         thermalManager.startMonitoring()
     }
 
-    private fun applyQualcommTweaks() {}
-    private fun applyMediatekTweaks() {}
-
-    private fun applyBatteryStabilizer() {
-        wakeLock = powerManager.newWakeLock(
-            PowerManager.PARTIAL_WAKE_LOCK,
-            "NSlocal:BatteryStabilizer"
-        )
-        if (!wakeLock!!.isHeld) {
-            wakeLock!!.acquire(10 * 60 * 1000L)
-        }
-    }
+    private fun applyQualcomm() {}
+    private fun applyMediatek() {}
 
     fun releaseAll() {
         thermalManager.stopMonitoring()
-        thermalManager.restoreNormalSettings()
-        wakeLock?.let { if (it.isHeld) it.release() }
     }
 }
