ccf4b25818a7897f1e20bd49149d6055d6454c09
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-06 10:20:08 +0700

    Update PerformanceHelper.kt

--- app/src/main/java/com/nslocal/games/perf/PerformanceHelper.kt
+++ app/src/main/java/com/nslocal/games/perf/PerformanceHelper.kt
@@ -5,13 +5,17 @@ import android.os.Build
 
 class PerformanceHelper(private val ctx: Context) {
     val socName: String
-        get() = if (Build.VERSION.SDK_INT >= 31) Build.SOC_NAME else Build.HARDWARE
+        get() = Build.HARDWARE
 
     val isQualcomm: Boolean
-        get() = socName.contains("qcom", ignoreCase = true) || socName.contains("sdm", ignoreCase = true) || socName.contains("sm", ignoreCase = true)
+        get() = socName.contains("qcom", ignoreCase = true) ||
+                socName.contains("sdm", ignoreCase = true) ||
+                socName.contains("sm", ignoreCase = true)
 
-    val isMediatek: Boolean
-        get() = socName.contains("mt", ignoreCase = true) || socName.contains("mediatek", ignoreCase = true) || socName.contains("mtk", ignoreCase = true)
+    val isMediaTek: Boolean
+        get() = socName.contains("mt", ignoreCase = true) ||
+                socName.contains("mediatek", ignoreCase = true) ||
+                socName.contains("mtk", ignoreCase = true)
 
     val deviceModel: String get() = Build.MODEL
     val androidVersion: String get() = Build.VERSION.RELEASE
