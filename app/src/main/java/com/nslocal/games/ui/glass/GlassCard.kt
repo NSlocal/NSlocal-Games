f7dd0d255dec68bc992b682c52ea7534f4d3347b
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-06 10:08:53 +0700

    Update GlassCard.kt

--- app/src/main/java/com/nslocal/games/ui/glass/GlassCard.kt
+++ app/src/main/java/com/nslocal/games/ui/glass/GlassCard.kt
@@ -3,12 +3,13 @@ package com.nslocal.games.ui.glass
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import com.nslocal.games.R
 
 class GlassCard @JvmOverloads constructor(
     ctx: Context, attrs: AttributeSet? = null, defStyle: Int = 0
 ) : FrameLayout(ctx, attrs, defStyle) {
     init {
         setBackgroundResource(R.drawable.glass_panel)
-        setPadding(32, 24, 32, 24)
+        setPadding(24, 16, 24, 16)
     }
 }
