802ef0c497b7209d33da33da6a536a4257236ae8
NSlocal <nadyaillustrationartist@gmail.com>
2026-09-06 19:46:42 +0700

    Update BoostEngine.kt

--- app/src/main/java/com/nslocal/games/optimizer/BoostEngine.kt
+++ app/src/main/java/com/nslocal/games/optimizer/BoostEngine.kt
@@ -1,23 +1,9 @@
 package com.nslocal.games.optimizer
-
-import android.content.Context
-import com.nslocal.games.perf.PerformanceHelper
-import com.nslocal.games.perf.ThermalCoolingManager
+import android.content.Context; import com.nslocal.games.perf.PerformanceHelper; import com.nslocal.games.perf.ThermalCoolingManager
 
 class BoostEngine(private val ctx: Context) {
     private val perfHelper = PerformanceHelper(ctx)
     val thermalManager = ThermalCoolingManager(ctx)
-
-    fun applyAll() {
-        if (perfHelper.isQualcomm) applyQualcomm()
-        if (perfHelper.isMediaTek) applyMediatek()
-        thermalManager.startMonitoring()
-    }
-
-    private fun applyQualcomm() {}
-    private fun applyMediatek() {}
-
-    fun releaseAll() {
-        thermalManager.stopMonitoring()
-    }
+    fun applyAll() { if (perfHelper.isQualcomm){}; if (perfHelper.isMediaTek){}; thermalManager.startMonitoring() }
+    fun releaseAll() { thermalManager.stopMonitoring() }
 }
